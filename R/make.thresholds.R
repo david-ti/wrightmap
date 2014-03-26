631bacc6b8c55beae33eb4e4edff561d721477cd
Rebecca Freund <rlfreund@berkeley.edu>
2014-03-26 03:44:32 -0400

    minor fix

--- R/make.thresholds.R
+++ R/make.thresholds.R
@@ -7,7 +7,7 @@ make.thresholds.character <- function(item.params,design.matrix="normal",...) {
 	return(make.thresholds(CQmodel(show=item.params),...))
 }
 
-make.thresholds.CQmodel <- function(item.params,item.table,interactions,step.table,design.matrix = "normal",...) {
+make.thresholds.CQmodel <- function(item.params,item.table = NULL,interactions = NULL,step.table = NULL,design.matrix = "normal",...) {
 	deltas <- make.deltas(item.params,item.table,interactions,step.table)
 	message("Creating threshold parameters out of deltas")
 	return(make.thresholds(deltas,...))
