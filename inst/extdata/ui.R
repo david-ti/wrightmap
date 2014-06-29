4f5303a9172705bff3631461a8ab15d20e629b68
Rebecca Freund <rlfreund@berkeley.edu>
2014-06-29 14:29:23 -0400

    shiny updates

--- inst/extdata/ui.R
+++ inst/extdata/ui.R
@@ -2,25 +2,61 @@ library(shiny)
 
 # Define UI for application that plots random distributions 
 shinyUI(pageWithSidebar(
-  
   # Application title
-  headerPanel("Hello Shiny!"),
+  headerPanel("Wright Map"),
   
   # Sidebar with a slider input for number of observations
   sidebarPanel(
-    fileInput('eap', 'Choose Person Estimates File'),
-    fileInput('shw', 'Choose Show File'),
-    selectInput("p.type", "Person estimates type:", 
-                choices = c("EAP", "MLE", "WLE")),
-    sliderInput("throld","Threshold",min=.01,max = .99, value = .5, step = .01),
-    checkboxInput('use.hist', 'Histogram?', TRUE),
-    checkboxInput('show.thr.lab', 'Show Threshold Labels', FALSE),
-
-    sliderInput("cex", 
-                "Symbol size", 
-                min = 1,
-                max = 5, 
-                value = 2.5, step = .1)
+	wellPanel(
+    		checkboxInput('data', 'Show data options', TRUE),
+    		conditionalPanel(condition='input.data',
+			wellPanel(
+  				checkboxInput('files', 'Show file options', TRUE),
+  				conditionalPanel(condition='input.files',
+    					fileInput('eap', 'Choose Person Estimates File'),
+    					fileInput('shw', 'Choose Show File'),
+    					selectInput("p.type", "Person estimates type:", 
+                			choices = c("EAP", "MLE", "WLE"))
+         			)
+    			),
+    			wellPanel(
+    				checkboxInput('table', 'Show table options', FALSE),
+    				conditionalPanel(condition='input.table',
+    					selectInput("type","Parameter type:", choices = c("Thresholds" = "thresholds","Deltas" = "deltas")),
+    					sliderInput("throld","Threshold",min=.01,max = .99, value = .5, step = .01)
+    				)
+    			)
+    		)
+	),
+	wellPanel(
+		checkboxInput('disp',"Show display options",FALSE),
+		conditionalPanel(condition='input.disp',
+			checkboxInput('autotitle',"Create title automatically",TRUE),
+			conditionalPanel(condition = "!input.autotitle",
+				textInput('title',"Title")
+			),
+			wellPanel(
+				checkboxInput('pers', 'Show person display options', FALSE),
+				conditionalPanel(condition='input.pers',
+    					checkboxInput('use.hist', 'Histogram?', TRUE)
+    				)
+    			),
+    			wellPanel(
+    				checkboxInput('item', 'Show item display options', FALSE),
+    				conditionalPanel(condition='input.item',
+    					checkboxInput('show.thr.lab', 'Show Threshold Labels', TRUE),
+					textInput('axis.logits',"Logit axis label","Logits"),
+		    			sliderInput(
+		    				"cex", 
+		                	"Symbol size", 
+		                	min = 1,
+		               	max = 5, 
+		                	value = 1.2, step = .1
+		            	)
+		       	)
+		    	)
+     	)
+     )
   ),
   
   # Show a plot of the generated distribution
