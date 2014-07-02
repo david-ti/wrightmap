d6d758564ea4bf3cbfc147aa8254820c228ed6e2
Rebecca Freund <rlfreund@berkeley.edu>
2014-07-02 13:45:23 -0400

    slopes + shiny updates

--- inst/extdata/ui.R
+++ inst/extdata/ui.R
@@ -7,60 +7,59 @@ shinyUI(pageWithSidebar(
   
   # Sidebar with a slider input for number of observations
   sidebarPanel(
+    selectInput("type","Parameter type:", choices = c("default","Thresholds" = "thresholds","Deltas" = "deltas")),
+    sliderInput("throld","Threshold",min=.01,max = .99, value = .5, step = .01),
 	wellPanel(
-    		checkboxInput('data', 'Show data options', TRUE),
-    		conditionalPanel(condition='input.data',
-			wellPanel(
-  				checkboxInput('files', 'Show file options', TRUE),
-  				conditionalPanel(condition='input.files',
-    					fileInput('eap', 'Choose Person Estimates File'),
-    					fileInput('shw', 'Choose Show File'),
-    					selectInput("p.type", "Person estimates type:", 
-                			choices = c("EAP", "MLE", "WLE"))
-         			)
-    			),
-    			wellPanel(
-    				checkboxInput('table', 'Show table options', FALSE),
-    				conditionalPanel(condition='input.table',
-    					selectInput("type","Parameter type:", choices = c("default","Thresholds" = "thresholds","Deltas" = "deltas")),
-    					sliderInput("throld","Threshold",min=.01,max = .99, value = .5, step = .01),
-    					uiOutput("item.table"),
-    					uiOutput("step.table"),
-    					uiOutput("interactions.table")
-    					
-    				)
-    			)
+    	checkboxInput('files', 'Show input options', TRUE),
+  		conditionalPanel(condition='input.files',
+  			selectInput('datatype',"Type of data", choices = c("ConQuest output" = "CQ","R object" = "R")),
+  			conditionalPanel(condition = "input.datatype == 'R'",
+  				textInput('thetas',"Name of thetas object"),
+  				textInput('thresholds',"Name of thresholds object"),
+  				textInput('slopes',"Name of slopes object (if not specified, assumed all are 1)")
+  			),
+  			conditionalPanel(condition="input.datatype == 'CQ'",
+    			fileInput('eap', 'Choose Person Estimates File'),
+    			fileInput('shw', 'Choose Show File'),
+    			selectInput("p.type", "Person estimates type:", choices = c("EAP", "MLE", "WLE"))
     		)
+    	)
+    ),
+    conditionalPanel(condition="input.datatype == 'CQ'",
+    	wellPanel(
+    		checkboxInput('table', 'Show table options', FALSE),
+    		conditionalPanel(condition='input.table',
+    			uiOutput("item.table"),
+    			uiOutput("step.table"),
+    			uiOutput("interactions.table")
+    		)
+    	)
 	),
 	wellPanel(
-		checkboxInput('disp',"Show display options",FALSE),
+		checkboxInput('disp',"Show general display options",FALSE),
 		conditionalPanel(condition='input.disp',
 			checkboxInput('autotitle',"Create title automatically",TRUE),
 			conditionalPanel(condition = "!input.autotitle",
 				textInput('title',"Title")
 			),
-			wellPanel(
-				checkboxInput('pers', 'Show person display options', FALSE),
-				conditionalPanel(condition='input.pers',
-    					checkboxInput('use.hist', 'Histogram?', TRUE)
-    				)
-    			),
-    			wellPanel(
-    				checkboxInput('item', 'Show item display options', FALSE),
-    				conditionalPanel(condition='input.item',
-    					checkboxInput('show.thr.lab', 'Show Threshold Labels', TRUE),
-					textInput('axis.logits',"Logit axis label","Logits"),
-		    			sliderInput(
+			checkboxInput('use.hist', 'Histogram?', TRUE)
+    	)
+    ),
+   wellPanel(
+    	checkboxInput('item', 'Show item display options', FALSE),
+    	conditionalPanel(condition='input.item',
+    		checkboxInput('show.thr.lab', 'Show Threshold Labels', TRUE),
+			textInput('axis.logits',"Logit axis label","Logits"),
+		    sliderInput(
 		    				"cex", 
 		                	"Symbol size", 
 		                	min = 1,
 		               	max = 5, 
 		                	value = 1.2, step = .1
-		            	)
-		       	)
-		    	)
-     	)
-     )
+		    )
+		)
+
+  	)
   ),
   
   # Show a plot of the generated distribution
