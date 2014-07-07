f80b92f4551663af3bf471bf65a9670768dd4631
Rebecca Freund <rlfreund@berkeley.edu>
2014-07-07 09:54:54 -0400

    trying to make jscolor work

--- R/jscolor.r
+++ R/jscolor.r
@@ -7,8 +7,8 @@
 #' @export
 jscolorInput <- function(id) {
     tagList(        
-        singleton(tags$head(tags$script(src = "shinysky/ss-jscolor.js"))),
-        singleton(tags$head(tags$script(src = "shinysky/jscolor/jscolor.js"))),
+        singleton(tags$head(tags$script(src = "WrightMap/ss-jscolor.js"))),
+        singleton(tags$head(tags$script(src = "WrightMap/jscolor/jscolor.js"))),
         tags$input(id = id, class = "color shiny-bound-input", onchange = sprintf("$('#%s').trigger('afterChange')",id)),
         tags$script(sprintf("$('#%s').trigger('afterChange')",id))
     )
