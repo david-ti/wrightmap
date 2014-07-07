59f5dfeebcf68c6b8d500124b18a2072255fcb5d
Rebecca Freund <rlfreund@berkeley.edu>
2014-07-07 10:04:29 -0400

    using fpath might work

--- R/jscolor.r
+++ R/jscolor.r
@@ -5,10 +5,12 @@
 #' @param id The id used to refer to the table input$id or output$id
 #'   
 #' @export
+fpath <- system.file("extdata", package="WrightMap")
+
 jscolorInput <- function(id) {
     tagList(        
-        singleton(tags$head(tags$script(src = "WrightMap/ss-jscolor.js"))),
-        singleton(tags$head(tags$script(src = "WrightMap/jscolor/jscolor.js"))),
+        singleton(tags$head(tags$script(src = file.path(fpath,"ss-jscolor.js")))),
+        singleton(tags$head(tags$script(src = file.path(fpath,"jscolor/jscolor.js")))),
         tags$input(id = id, class = "color shiny-bound-input", onchange = sprintf("$('#%s').trigger('afterChange')",id)),
         tags$script(sprintf("$('#%s').trigger('afterChange')",id))
     )
