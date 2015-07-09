599767d38669fcfb151297e89ab627605cc2f309
rfreund <rlfreund@berkeley.edu>
2015-07-09 15:36:28 -0400

    adjust text
    fixed text margins to adjust to scale

--- R/cutLines.R
+++ R/cutLines.R
@@ -1,10 +1,12 @@
 cutLines <- function(cutpoints = NULL,cut.left = 0, cut.right = 1, cut.lab.text = NULL, cut.lab.adj = c(0,1),...) {
 	
 	if(!is.null(cutpoints)) {
-	
-	segments(cut.left,cutpoints,cut.right * par("usr")[2], col = 'grey70', lwd = 1.5, lend = 2)
 		
-		text(.1 + cut.lab.adj[1] * (par("usr")[2] - .2),cutpoints + (.5 - cut.lab.adj[2])*.1,labels = cut.lab.text,cex= .6, adj = cut.lab.adj)
+		width <- par("usr")[2]
+	
+	segments(cut.left * width,cutpoints,cut.right * width, col = 'grey70', lwd = 1.5, lend = 2)
+		
+		text(width *(.01  + cut.lab.adj[1] * .98),cutpoints + (.5 - cut.lab.adj[2])*.1,labels = cut.lab.text,cex= .6, adj = cut.lab.adj)
 		
 		}
 }
